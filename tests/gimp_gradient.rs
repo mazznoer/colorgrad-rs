ad2e141f5eb60ae50acd44dfe3ab596e11dfb5ee
Nor Khasyatillah <mazznoer@ymail.com>
2021-06-16 21:00:53 +0700

    Add gimp gradient tests

--- tests/gimp_gradient.rs
+++ tests/gimp_gradient.rs
@@ -42,4 +42,48 @@ fn parse_gimp_gradients() {
     assert_eq!(grad.at(0.51).rgba_u8(), (0, 0, 255, 255));
     assert_eq!(grad.at(0.75).rgba_u8(), (0, 0, 255, 255));
     assert_eq!(grad.at(1.00).rgba_u8(), (0, 0, 255, 255));
+
+    // Coloring type: HSV CCW (white to blue)
+    let ggr = "GIMP Gradient\nName: My Gradient\n1\n0 0.5 1 1 1 1 1 0 0 1 1 0 1 0 0";
+    let (grad, _) = parse_ggr(BufReader::new(ggr.as_bytes()), &red, &blue).unwrap();
+
+    assert_eq!(grad.at(0.0).rgba_u8(), (255, 255, 255, 255));
+    assert_eq!(grad.at(0.5).rgba_u8(), (128, 255, 128, 255));
+    assert_eq!(grad.at(1.0).rgba_u8(), (0, 0, 255, 255));
+
+    // Coloring type: HSV CW (white to blue)
+    let ggr = "GIMP Gradient\nName: My Gradient\n1\n0 0.5 1 1 1 1 1 0 0 1 1 0 2 0 0";
+    let (grad, _) = parse_ggr(BufReader::new(ggr.as_bytes()), &red, &blue).unwrap();
+
+    assert_eq!(grad.at(0.0).rgba_u8(), (255, 255, 255, 255));
+    assert_eq!(grad.at(0.5).rgba_u8(), (255, 128, 255, 255));
+    assert_eq!(grad.at(1.0).rgba_u8(), (0, 0, 255, 255));
+}
+
+#[test]
+fn invalid_format() {
+    let col = Color::default();
+
+    let test_data = vec![
+        ("GIMP Pallete\n9", "invalid header (line 1)"),
+        ("GIMP Gradient\n6", "invalid header (line 2)"),
+        (
+            "GIMP Gradient\nName: Gradient\nx",
+            "invalid header (line 3)",
+        ),
+        (
+            "GIMP Gradient\nName: Gradient\n1\n0 0.5 1",
+            "invalid segment (line 4)",
+        ),
+        (
+            "GIMP Gradient\nName: Gradient\n3\n0 0.5 1 0 0 0 1 1 1 1 1 0 0 0 0",
+            "wrong segments count (line 3)",
+        ),
+        ("GIMP Gradient\nName: Gradient\n0", "no segment (line 4)"),
+    ];
+
+    for (ggr, err_msg) in test_data {
+        let res = parse_ggr(BufReader::new(ggr.as_bytes()), &col, &col);
+        assert_eq!(res.unwrap_err().to_string(), err_msg);
+    }
 }

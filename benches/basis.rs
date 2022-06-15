d289637bf53c68a834fddc1d4b64e28b77a81ef6
Nor Khasyatillah <mazznoer@ymail.com>
2022-06-16 02:20:38 +0700

    more benchmarks

--- benches/basis.rs
+++ benches/basis.rs
@@ -1,21 +1,59 @@
+use colorgrad::BlendMode;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-fn bench_basis(c: &mut Criterion) {
-    let grad = colorgrad::CustomGradient::new()
+fn build_gradient(mode: BlendMode) -> colorgrad::Gradient {
+    colorgrad::CustomGradient::new()
         .html_colors(&[
             "#8dd3c7", "#ffffb3", "#bebada", "#fb8072", "#80b1d3", "#fdb462", "#b3de69", "#fccde5",
             "#d9d9d9", "#bc80bd", "#ccebc5", "#ffed6f",
         ])
+        .mode(mode)
         .interpolation(colorgrad::Interpolation::Basis)
         .build()
-        .unwrap();
+        .unwrap()
+}
 
-    c.bench_function("basis", |b| {
+fn bench_basis_rgb(c: &mut Criterion) {
+    let grad = build_gradient(BlendMode::Rgb);
+    c.bench_function("BasisGradient (rgb)", |b| {
         b.iter(|| {
             grad.at(black_box(0.6));
         })
     });
 }
 
-criterion_group!(basis, bench_basis);
-criterion_main!(basis);
+fn bench_basis_lrgb(c: &mut Criterion) {
+    let grad = build_gradient(BlendMode::LinearRgb);
+    c.bench_function("BasisGradient (linear-rgb)", |b| {
+        b.iter(|| {
+            grad.at(black_box(0.6));
+        })
+    });
+}
+
+fn bench_basis_oklab(c: &mut Criterion) {
+    let grad = build_gradient(BlendMode::Oklab);
+    c.bench_function("BasisGradient (oklab)", |b| {
+        b.iter(|| {
+            grad.at(black_box(0.6));
+        })
+    });
+}
+
+fn bench_basis_hsv(c: &mut Criterion) {
+    let grad = build_gradient(BlendMode::Hsv);
+    c.bench_function("BasisGradient (hsv)", |b| {
+        b.iter(|| {
+            grad.at(black_box(0.6));
+        })
+    });
+}
+
+criterion_group!(
+    basis_gradient,
+    bench_basis_rgb,
+    bench_basis_lrgb,
+    bench_basis_oklab,
+    bench_basis_hsv
+);
+criterion_main!(basis_gradient);
--- benches/catmull_rom.rs
+++ benches/catmull_rom.rs
@@ -1,21 +1,59 @@
+use colorgrad::BlendMode;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-fn bench_catmull_rom(c: &mut Criterion) {
-    let grad = colorgrad::CustomGradient::new()
+fn build_gradient(mode: BlendMode) -> colorgrad::Gradient {
+    colorgrad::CustomGradient::new()
         .html_colors(&[
             "#8dd3c7", "#ffffb3", "#bebada", "#fb8072", "#80b1d3", "#fdb462", "#b3de69", "#fccde5",
             "#d9d9d9", "#bc80bd", "#ccebc5", "#ffed6f",
         ])
+        .mode(mode)
         .interpolation(colorgrad::Interpolation::CatmullRom)
         .build()
-        .unwrap();
+        .unwrap()
+}
 
-    c.bench_function("catmull_rom", |b| {
+fn bench_catmull_rom_rgb(c: &mut Criterion) {
+    let grad = build_gradient(BlendMode::Rgb);
+    c.bench_function("CatmullRomGradient (rgb)", |b| {
         b.iter(|| {
             grad.at(black_box(0.6));
         })
     });
 }
 
-criterion_group!(catmull_rom, bench_catmull_rom);
-criterion_main!(catmull_rom);
+fn bench_catmull_rom_lrgb(c: &mut Criterion) {
+    let grad = build_gradient(BlendMode::LinearRgb);
+    c.bench_function("CatmullRomGradient (linear-rgb)", |b| {
+        b.iter(|| {
+            grad.at(black_box(0.6));
+        })
+    });
+}
+
+fn bench_catmull_rom_oklab(c: &mut Criterion) {
+    let grad = build_gradient(BlendMode::Oklab);
+    c.bench_function("CatmullRomGradient (oklab)", |b| {
+        b.iter(|| {
+            grad.at(black_box(0.6));
+        })
+    });
+}
+
+fn bench_catmull_rom_hsv(c: &mut Criterion) {
+    let grad = build_gradient(BlendMode::Hsv);
+    c.bench_function("CatmullRomGradient (hsv)", |b| {
+        b.iter(|| {
+            grad.at(black_box(0.6));
+        })
+    });
+}
+
+criterion_group!(
+    catmull_rom_gradient,
+    bench_catmull_rom_rgb,
+    bench_catmull_rom_lrgb,
+    bench_catmull_rom_oklab,
+    bench_catmull_rom_hsv
+);
+criterion_main!(catmull_rom_gradient);

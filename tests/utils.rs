185a8d56d68feb7385c998b08a79d240448315aa
Nor Khasyatillah <mazznoer@ymail.com>
2026-02-06 23:05:33 +0700

    fix tests

--- tests/utils.rs
+++ tests/utils.rs
@@ -1,8 +1,17 @@
+use std::borrow::Borrow;
+
 use colorgrad::Color;
 
 #[allow(dead_code)]
-pub fn colors2hex(colors: impl Iterator<Item = Color>) -> Vec<String> {
-    colors.map(|c| c.to_css_hex()).collect()
+pub fn colors2hex<T, U>(colors: T) -> Vec<String>
+where
+    T: IntoIterator<Item = U>,
+    U: Borrow<Color>,
+{
+    colors
+        .into_iter()
+        .map(|c| c.borrow().to_css_hex())
+        .collect()
 }
 
 #[macro_export]
